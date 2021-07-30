cc8641e48916f7419fdb985e0e0676d2ed28d38e
Leela165 <88185222+Leela165@users.noreply.github.com>
2021-07-30 14:29:39 +0530

    commands added

--- scripting.sh
+++ scripting.sh
@@ -2,3 +2,7 @@ This is sample linux code
 echo " hello "
 echo "hi"
 Linux is the operating system. it is free to use
+we have more commands in linux
+cd---> changes the directory
+pwd ---> present working directory
+ls ---> lists the directories and files
